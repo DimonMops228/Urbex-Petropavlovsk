2609422bb5b7a29c636aac7caaac06b6d000c44a
DimonMops228 <beznosovdima1806@gmail.com>
2026-06-17 13:49:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function filterObjects(category) {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        if (category === "all" || card.dataset.category === category) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
